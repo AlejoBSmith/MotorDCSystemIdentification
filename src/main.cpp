05b762dc60bd630455bebd6a3a788173dbc94063
Alejandro <65981114+AlejoBSmith@users.noreply.github.com>
2024-09-16 20:03:34 -0500

    Mejora codigo y agregar gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <movingAvg.h>
-#include <StringSplitter.h>
 
 // Pines control motor
 const int pwmPin = 9;   // Enable pin (PWM input)
@@ -14,18 +13,23 @@ const int encoderB = 3; // Encoder channel B
 //Controlador
 bool inicio = 1;
 int sumaPID, a = 0;
-int accionProp, accionDeriv, accionInteg, error, tiempo, tiempoinicio, tiempoanterior, referencia, encoderTicksActual, encoderTicksAnterior, deltaencoderTicks, sumerror, time_delay, TiempoCicloPromedio = 0;
-float Kp, Ki, Kd, tiempociclo, RPM, RPM1erFilt, RPMPromedio, RPMPromedioanterior = 0;
+float controladorOUT = 0;
+float x[2] = {0}; // Input samples
+float y[2] = {0}; // Output samples
+int accionProp, accionDeriv, RPMMedian, accionInteg, error, tiempo, tiempoinicio, tiempoanterior, referencia, encoderTicksActual, encoderTicksAnterior, deltaencoderTicks, sumerror, time_delay, TiempoCicloPromedio = 0;
+float Kp, Ki, Kd, tiempociclo, RPM, RPM1erFilt, RPMPromedio, RPMPromedioanterior, Prom1 = 0;
 String DatoSerial;
-int avgwindowsize = 1; //El lag que introduce un movingAvg es (N-1)/2 muestras
-int delayintencional = 100;
-movingAvg RPM1(avgwindowsize);
-movingAvg RPMAvg(avgwindowsize);
-movingAvg TiempoCicloAvg(10);
 
+// IDENTIFICACIÓN DE SISTEMA
+int avgwindowsize = 5; // Variar este parámetro. El lag que introduce un movingAvg es (N-1)/2 muestras
+int delayintencional = 10; // Variar este parámetro
+
+movingAvg RPMAvg(avgwindowsize);
+movingAvg TiempoCicloAvg(20);
+
+// Variables para el conteo de pulsos del encoder
 volatile float encoderTicks = 0;
 int lastEncoded = 0;
-String StringEntrada;
 
 void updateEncoder() {
   // Se leen los encoder A y B para luego combinarlos en un solo número
@@ -58,7 +62,6 @@ void setup() {
   Ki = 0.075;
   Kd = 4;
   a = 0; //variable auxiliar para cambio de referencia (setpoint)
-  RPM1.begin();
   RPMAvg.begin();
   TiempoCicloAvg.begin();
   randomSeed(analogRead(0)); //Inicializa aleatoriamente el generador de números aleatorios
@@ -66,7 +69,6 @@ void setup() {
 
 void loop() {
   if(a==0){  //Genera referencias variables para entrenamiento red neuronal
-    //referencia=100; // Para encontrar el ruido
     referencia=random(70,160); // Deben operar el motor en su zona lineal! Las FT son lineales
     tiempoinicio=millis();
     a=1;
@@ -74,22 +76,24 @@ void loop() {
   tiempoanterior=tiempo;
   tiempo=millis();
   tiempociclo=tiempo-tiempoanterior;
-  if((tiempo-tiempoinicio>3000) && a==1){
+  if((tiempo-tiempoinicio>3000) && a==1){ //mantiene la referencia por este tiempo
     a=0;
   }
-  //encoderTicksAnterior=encoderTicksActual;
-  //encoderTicksActual=encoderTicks;
-  //deltaencoderTicks=encoderTicksActual-encoderTicksAnterior;
-  //RPM=deltaencoderTicks/(tiempociclo)*78.125;
-  RPM=encoderTicks/(tiempociclo)*78.125;
+
+  RPM=encoderTicks/(TiempoCicloPromedio)*78.125; //Factor de conversión para pasar de Ticks a RPM. Esto depende de su encoder.
+  RPMPromedio=RPMAvg.reading(RPM);
   encoderTicks=0;
-  RPMPromedioanterior = RPMPromedio;
-  RPM1erFilt=RPM1.reading(RPM);
-  RPMPromedio=RPMAvg.reading(RPM1erFilt);
   TiempoCicloPromedio=TiempoCicloAvg.reading(tiempociclo);
   error=referencia-RPMPromedio;
-  accionProp=error*Kp;
-  accionProp=constrain(accionProp,0,255);
+
+  //Definición del controlador en Z
+  //controladorOUT = 1.0582*error+1.037*y[0]-0.4021*y[1];
+  //controladorOUT = constrain(controladorOUT,0,255);
+  //x[0] = error; //error es la entrada U
+  //y[1] = y[0];
+  //y[0] = controladorOUT; //controlador OUT es la salida Y
+  //analogWrite(pwmPin, controladorOUT);
+  
   analogWrite(pwmPin, referencia);
 
   Serial.print(int(referencia));
